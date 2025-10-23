7fec59e7badfe46aa208c45e6a77bba5761f51d7
noha <nohaelsrogy1@gmil.com>
2025-10-24 02:10:07 +0300

    رسالة التغيير

--- hw_s3_flutter/lib/heigh_section.dart
+++ hw_s3_flutter/lib/heigh_section.dart
@@ -11,57 +11,61 @@ class _HeightItemState extends State<HeightItem> {
   double slider = 0;
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: Color(0xff16162e),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'HEIGHT',
-            style: TextStyle(
-                color: Colors.grey, fontWeight: FontWeight.w100, fontSize: 25),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                '174',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 35),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 15),
-                child: Text(
-                  'cm',
+    return Expanded(
+      child: Card(
+        color: Color(0xff16162e),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'HEIGHT',
+              style: TextStyle(
+                  color: Colors.grey,
+                  fontWeight: FontWeight.w100,
+                  fontSize: 25),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  '174',
                   style: TextStyle(
                       color: Colors.white,
-                      fontWeight: FontWeight.w100,
-                      fontSize: 10),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 35),
                 ),
-              ),
-            ],
-          ),
-          SliderTheme(
-            data: SliderThemeData(
-                trackHeight: 2,
-                thumbShape: RoundSliderThumbShape(enabledThumbRadius: 16)),
-            child: Slider(
-              value: slider,
-              thumbColor: Color(0xffed0c53),
-              activeColor: Colors.white,
-              onChanged: (value) {
-                setState(() {
-                  slider = value;
-                });
-              },
+                Padding(
+                  padding: const EdgeInsets.only(top: 15),
+                  child: Text(
+                    'cm',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w100,
+                        fontSize: 10),
+                  ),
+                ),
+              ],
             ),
-          ),
-          SizedBox(
-            height: 16,
-          )
-        ],
+            SliderTheme(
+              data: SliderThemeData(
+                  trackHeight: 2,
+                  thumbShape: RoundSliderThumbShape(enabledThumbRadius: 16)),
+              child: Slider(
+                value: slider,
+                thumbColor: Color(0xffed0c53),
+                activeColor: Colors.white,
+                onChanged: (value) {
+                  setState(() {
+                    slider = value;
+                  });
+                },
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            )
+          ],
+        ),
       ),
     );
   }
--- hw_s3_flutter/lib/main.dart
+++ hw_s3_flutter/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:hw_s3_flutter/MaleFamaleCard.dart';
+import 'package:hw_s3_flutter/Home_page.dart';
+import 'package:hw_s3_flutter/Male_Famale_Section.dart';
 import 'package:hw_s3_flutter/heigh_section.dart';
-import 'package:hw_s3_flutter/weight%20and%20heigh%20section.dart';
+import 'package:hw_s3_flutter/weight_age_section.dart';
 
 void main() {
   runApp(S3());
@@ -13,50 +14,7 @@ class S3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Color(0xff03051a),
-        appBar: AppBar(
-          backgroundColor: Color(0xff04061c),
-          title: Text(
-            'BMI CALCULATOR',
-            style: TextStyle(color: Colors.white),
-          ),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-          child: Column(
-            children: [
-              MaleFemale(),
-              SizedBox(
-                height: 10,
-              ),
-              HeightItem(),
-              SizedBox(
-                height: 10,
-              ),
-              WeightAndHeghtSection(),
-              SizedBox(
-                height: 41,
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text(
-                  'CALCULATE',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 26,
-                    color: Colors.white,
-                  ),
-                ),
-                style: ElevatedButton.styleFrom(
-                    minimumSize: Size(400, 80),
-                    primary: Color(0xffed0c53),
-                    shape: BeveledRectangleBorder()),
-              )
-            ],
-          ),
-        ),
-      ),
+      home: HomeBage(),
     );
   }
 }
